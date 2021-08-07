7e5ba1df09427093460e06f96b9b01f1eae16dde
Alexandru Scurtu <sasha.scurtu@gmail.com>
2021-08-07 16:13:11 +0300

    raphael: Remove livedisplay
    Change-Id: Iedc8816d54ce17fbb19c0e486cdee02a8c0b7619

--- device.mk
+++ device.mk
@@ -260,11 +260,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_raphael
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service.xiaomi_raphael
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
